27e3fe7f64fb49961e07e447f15c642fa9d397a6
Xiangyu Lyu <xiangyu.lyu@stud.tu-darmstadt.de>
2025-07-18 21:57:00 +0200

    Update default test directory path in prediction script for consistency

--- wettbewerb.py
+++ wettbewerb.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-此文件不应被修改，由我们提供并重置。
+Diese Datei sollte nicht verändert werden und wird von uns gestellt und zurückgesetzt.
 
-用于加载和保存文件的函数
+Funktionen zum Laden und Speichern der Dateien
 """
 __author__ = "Maurice Rohr und Dirk Schweickard"
 
@@ -13,36 +13,36 @@ import numpy as np
 import os
 
 
-### 注意！不要修改此函数。
-def load_references(folder: str = './shared_data/training', idx: int = 0) -> Tuple[List[str], List[List[str]],
+### Achtung! Diese Funktion nicht veraendern.
+def load_references(folder: str = '../training', idx: int = 0) -> Tuple[List[str], List[List[str]],
                                                           List[np.ndarray],  List[float],
                                                           List[str], List[Tuple[bool,float,float]]]:
     """
-    从 .mat（测量数据）和 .csv（标签）文件中读取参考数据。
+    Liest Referenzdaten aus .mat (Messdaten) und .csv (Label) Dateien ein.
     Parameters
     ----------
     folder : str, optional
-        训练数据的位置。默认值 '../training'。
-    idx : int, optional
-        开始加载数据的索引。
-        例如 idx=10 意味着将加载数据点 10 到 109（包含）
-        如果从索引开始剩余文件少于100个，则只加载这些文件。
+        Ort der Trainingsdaten. Default Wert '../training'.
+    idx : int, optinal
+        Index, ab dem das Laden der Daten starten soll.
+        z.B. idx=10 bedeutet es werden die Datenpunkte 10 bis 109 (einschließlich) geladen
+        Falls weniger als 100 Dateien ab Index übrig, werden nur diese geladen.
 
     Returns
     -------
     ids : List[str]
-        记录ID列表
+        Liste von ID der Aufnahmen
     channels : List[List[str]]
-        每个记录的可用通道列表
+        Liste der vorhandenen Kanäle per Aufnahme
     data :  List[ndarray]
-        每个记录的数据列表
+        Liste der Daten pro Aufnahme
     sampling_frequencies : List[float]
-        采样频率列表。
+        Liste der Sampling-Frequenzen.
     reference_systems : List[str]
-        参考系统列表。"LE", "AR", "Sz"（附加信息）
+        Liste der Referenzsysteme. "LE", "AR", "Sz" (Zusatz-Information)
     """
     
-    # 初始化列表 ids, channels, data, sampling_frequencies, reference_systems 和 eeg_labels
+    # Initialisiere Listen ids, channels, data, sampling_frequencies, refernece_systems und eeg_labels
     ids: List[str] = []
     channels: List[List[str]] = []
     data: List[np.ndarray] = []
@@ -50,15 +50,15 @@ def load_references(folder: str = './shared_data/training', idx: int = 0) -> Tup
     reference_systems: List[str] = []
     eeg_labels: List[Tuple[bool,float,float]] = []
     
-    # 从文件夹创建数据集并用数据填充列表
+    # Erzeuge Datensatz aus Ordner und fülle Listen mit Daten
     dataset = EEGDataset(folder)
-    data_to_load = 100  # 加载最多100个记录
+    data_to_load = 100  # lade bis zu 100 Aufnahmen
     if(len(dataset)-idx < data_to_load):
         end = len(dataset)-idx
     else:
         end = data_to_load
     if(idx >= len(dataset)):
-        print("数据集小于提供的索引")
+        print("Dataset is smaller than the provided idx")
         return None
     for i in range(idx, idx+end):
         ids.append(dataset[i][0])
@@ -68,39 +68,39 @@ def load_references(folder: str = './shared_data/training', idx: int = 0) -> Tup
         reference_systems.append(dataset[i][4])
         eeg_labels.append(dataset[i][5])
         
-    # 显示加载了多少数据
-    print("{}\t 个文件已加载。".format(len(ids)))
+    # Zeige an wie viele Daten geladen wurden
+    print("{}\t Dateien wurden geladen.".format(len(ids)))
     return ids, channels, data, sampling_frequencies, reference_systems, eeg_labels
 
-### 注意！不要修改此类。
+### Achtung! Diese Klasse nicht veraendern.
 class EEGDataset:
     def __init__(self,folder:str) -> None:
-        """此类表示一个EEG数据集。
+        """Diese Klasse stellt einen EEG Datensatz dar.
         
-        用法：
-            用以下方式创建新数据集（不加载所有数据）
+        Verwendung:
+            Erzeuge einen neuen Datensatz (ohne alle Daten zu laden) mit
             dataset = EEGDataset("../training/")
-            len(dataset) # 返回数据集大小
-            dataset[0] # 返回数据集中第一个元素，包含 (id, channels, data, sampling_frequency, reference_system, eeg_label)
-            it = iter(dataset) # 返回数据集的迭代器，
-            next(it) # 返回下一个元素，直到所有数据被获取一次
-            for item in dataset: # 遍历整个数据集一次
+            len(dataset) # gibt Größe des Datensatzes zurück
+            dataset[0] # gibt erstes Element aus Datensatz zurück bestehend aus (id, channels, data, sampling_frequency, reference_system, eeg_label)
+            it = iter(dataset) # gibt einen iterator zurück auf den Datensatz,
+            next(it) # gibt nächstes Element zurück bis alle Daten einmal geholt wurden
+            for item in dataset: # iteriert einmal über den gesamten Datensatz
                 (id, channels, data, sampling_frequency, reference_system, eeg_label) = item
-                # 计算
+                # Berechnung
 
         Args:
-            folder (str): 包含由.mat文件和REFERENCE.csv文件组成的数据集的文件夹
+            folder (str): Ordner in dem der Datensatz bestehend aus .mat-Dateien und einer REFERENCE.csv Datei liegt
         """
-        assert isinstance(folder, str), "参数folder必须是字符串，但给出了{}".format(type(folder))
-        assert os.path.exists(folder), '参数folder不存在！'
-        # 初始化ids和labels列表
+        assert isinstance(folder, str), "Parameter folder muss ein string sein aber {} gegeben".format(type(folder))
+        assert os.path.exists(folder), 'Parameter folder existiert nicht!'
+        # Initialisiere Listen für ids und labels
         self._folder = folder
         self._ids: List[str] = []
         self._eeg_labels: List[Tuple[bool,float,float]] = []
-        # 加载references文件
+        # Lade references Datei
         with open(os.path.join(folder, 'REFERENCE.csv')) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-            # 遍历每一行
+            # Iteriere über jede Zeile
             for row in csv_reader:
                 self._ids.append(row[0])
                 self._eeg_labels.append((int(row[1]),float(row[2]),float(row[3])))
@@ -111,7 +111,7 @@ class EEGDataset:
     def __getitem__(self,idx) -> Tuple[str, List[str],
                                     np.ndarray,  float,
                                     str, Tuple[bool,float,float]]:
-        # 加载Matlab文件
+        #Lade Matlab-Datei
         eeg_data = sio.loadmat(os.path.join(self._folder, self._ids[idx] + '.mat'),simplify_cells=True)
         ch_names = eeg_data.get('channels')
         channels = [x.strip(' ') for x in ch_names] 
@@ -130,35 +130,35 @@ class EEGDataset:
 
 
 
-### 注意！不要修改此函数。
+### Achtung! Diese Funktion nicht veraendern.
 #predictions = {"id":id,"seizure_present":seizure_present,"seizure_confidence":seizure_confidence,
 #                   "onset":onset,"onset_confidence":onset_confidence,"offset":offset,
 #                   "offset_confidence":offset_confidence}
 def save_predictions(predictions: List[Dict[str,Any]], folder: str=None, teamID: str=None) -> None:
     """
-    函数将给定的预测保存到名为PREDICTIONS.csv的CSV文件中。
-    所有可选预测都用默认值替换。
+    Funktion speichert the gegebenen predictions in eine CSV-Datei mit dem name PREDICTIONS.csv. 
+    Alle Optionalen Vorherhsagen werden mit Standardwerten ersetzt.
     Parameters
     ----------
     predictions : List[Dict[str,Any]]
-        字典列表。每个字典包含字段"id","seizure_present",
-                "seizure_confidence"（可选）,"onset","onset_confidence"（可选）,
-                "offset"（可选）,"offset_confidence"（可选）
+        Liste aus dictionaries. Jedes Dictionary enthält die Felder "id","seizure_present",
+                "seizure_confidence" (optional),"onset","onset_confidence" (optional),
+                "offset" (optional),"offset_confidence" (optional)
 	folder : str
-		预测的保存位置
+		Speicherort der predictions
     Returns
     -------
     None.
 
     """    
-	# 检查参数
+	# Check Parameter
     assert isinstance(predictions, list), \
-        "参数predictions必须是列表，但给出了{}。".format(type(predictions))
-    assert len(predictions) > 0, '参数predictions必须是非空列表。'
+        "Parameter predictions muss eine Liste sein aber {} gegeben.".format(type(predictions))
+    assert len(predictions) > 0, 'Parameter predictions muss eine nicht leere Liste sein.'
     assert isinstance(predictions[0], dict), \
-        "列表predictions的元素必须是字典，但给出了{}。".format(type(predictions[0]))
+        "Elemente der Liste predictions muss ein Dictionary sein aber {} gegeben.".format(type(predictions[0]))
     assert "id" in predictions[0], \
-        "预测必须有ID，但字典中没有相应的键"
+        "Prädiktionen müssen eine ID besitzen, aber Key in Dictionary nicht vorhanden"
 	
     if folder==None:
         file = "PREDICTIONS.csv"
@@ -167,15 +167,15 @@ def save_predictions(predictions: List[Dict[str,Any]], folder: str=None, teamID:
             file = os.path.join(folder, "PREDICTIONS.csv")
         else:
             file = os.path.join(folder, "PREDICTIONS"+"_"+teamID+".csv")
-    # 检查文件是否已存在，如果是则删除文件
+    # Check ob Datei schon existiert wenn ja loesche Datei
     if os.path.exists(file) and teamID is None:
         os.remove(file)
 
     with open(file, mode='w', newline='') as predictions_file:
 
-        # 初始化CSV写入器来写入文件
+        # Init CSV writer um Datei zu beschreiben
         predictions_writer = csv.writer(predictions_file, delimiter=',')
-        # 遍历每个预测
+        # Iteriere über jede prediction
         header=["id","seizure_present","seizure_confidence","onset","onset_confidence","offset","offset_confidence"]
         predictions_writer.writerow(header)
         for prediction in predictions:
@@ -187,27 +187,27 @@ def save_predictions(predictions: List[Dict[str,Any]], folder: str=None, teamID:
             _offset = prediction.get("offset",999999.0)
             _offset_confidence = prediction.get("offset_confidence",0.0)
             predictions_writer.writerow([_id,_seizure_present,_seizure_confidence,_onset,_onset_confidence,_offset,_offset_confidence])
-        # 输出保存了多少个标签（预测）的信息
-        print("{}\t 个标签已写入。".format(len(predictions)))
+        # Gebe Info aus wie viele labels (predictions) gespeichert werden
+        print("{}\t Labels wurden geschrieben.".format(len(predictions)))
         
 
 def get_3montages(channels: List[str], data: np.ndarray) -> Tuple[List[str],np.ndarray,bool]:
     """
-    函数从给定的导联（蒙太奇）计算3个蒙太奇 Fp1-F3, Fp2-F4, C3-P3
-    到同一参考电极。如果没有所有必需的电极，相应的信号将被0替换。
+    Funktion berechnet die 3 Montagen Fp1-F3, Fp2-F4, C3-P3 aus den gegebenen Ableitungen (Montagen)
+    zur selben Referenzelektrode. Falls nicht alle nötigen Elektroden vorhanden sind, wird das entsprechende Signal durch 0 ersetzt. 
     ----------
     channels : List[str]
-        通道名称，例如 Fp1, Cz, C3
+        Namen der Kanäle z.B. Fp1, Cz, C3
 	data : ndarray
-		通道数据
+		Daten der Kanäle
     Returns
     -------
     montages : List[str]
-        蒙太奇名称 ["Fp1-F3", "Fp2-F4", "C3-P3"]
+        Namen der Montagen ["Fp1-F3", "Fp2-F4", "C3-P3"]
     montage_data : ndarray
-        蒙太奇数据
+        Daten der Montagen
     montage_missing : bool
-        如果一个或多个蒙太奇缺失则为1，否则为0
+        1 , falls eine oder mehr Montagen fehlt, sonst 0
 
     """   
     montages = []
@@ -261,21 +261,21 @@ def get_3montages(channels: List[str], data: np.ndarray) -> Tuple[List[str],np.n
 
 def get_6montages(channels: List[str], data: np.ndarray) -> Tuple[List[str],np.ndarray,bool]:
     """
-    函数从给定的导联（蒙太奇）计算6个蒙太奇 Fp1-F3, Fp2-F4, C3-P3, F3-C3, F4-C4, C4-P4
-    到同一参考电极。如果没有所有必需的电极，相应的信号将被0替换。
+    Funktion berechnet die 6 Montagen Fp1-F3, Fp2-F4, C3-P3, F3-C3, F4-C4, C4-P4 aus den gegebenen Ableitungen (Montagen)
+    zur selben Referenzelektrode. Falls nicht alle nötigen Elektroden vorhanden sind, wird das entsprechende Signal durch 0 ersetzt. 
     ----------
     channels : List[str]
-        通道名称，例如 Fp1, Cz, C3
+        Namen der Kanäle z.B. Fp1, Cz, C3
 	data : ndarray
-		通道数据
+		Daten der Kanäle
     Returns
     -------
     montages : List[str]
-        蒙太奇名称 ["Fp1-F3", "Fp2-F4", "C3-P3", "F3-C3", "F4-C4", "C4-P4"]
+        Namen der Montagen ["Fp1-F3", "Fp2-F4", "C3-P3", "F3-C3", "F4-C4", "C4-P4"]
     montage_data : ndarray
-        蒙太奇数据
+        Daten der Montagen
     montage_missing : bool
-        如果一个或多个蒙太奇缺失则为1，否则为0
+        1 , falls eine oder mehr Montagen fehlt, sonst 0
 
     """  
     montages = []
